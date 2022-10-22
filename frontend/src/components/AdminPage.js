bcda679d8f834d2f9b3bd373a8b6b5a0346e78e2
Alton <altondsilva3010@gmail.com>
2022-10-22 16:18:00 +0530

    admin page

--- frontend/src/components/AdminPage.js
+++ frontend/src/components/AdminPage.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const AdminPage = () => {
   console.log("Admin page");
-  return <div>AdminPage</div>;
+  return (
+    <div>
+      <h1>Admin Page</h1>
+    </div>
+  );
 };
 
 export default AdminPage;
